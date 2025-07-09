609af2b0184e7fc1cab03503412be03a608a36c4
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-09 19:07:44 -0300

    Update bio-links.blade.php

--- resources/views/bio-links.blade.php
+++ resources/views/bio-links.blade.php
@@ -1,2 +1,30 @@
 <x-layout.app>
-<\x-layout.app>
+    <x-container>
+        
+        
+
+        <div class="text-center space-y-3 w-2/3">
+
+            <x-img src="/storage/{{ $user->photo }}" alt="Profile Picture"/>
+            <div class="font-bold text-2xl tracking-wider">{{$user->name}}</div>
+            <div class="text-sm italic opacity-80">{{$user->description}}</div>
+
+            <ul class="space-y-2 text-center flex flex-col">
+                @foreach ($user->links as $link)
+                    <li class="flex items-center gap-2 justify-center">
+
+                       
+
+                        <x-button href="{{ $link->link}}" block outline info target="_blank">
+                            {{ $link->name }}
+                        </x-button>
+                       
+                       
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+    </x-container>
+    
+</x-layout.app>
