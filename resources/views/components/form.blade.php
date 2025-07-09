83f7f41fab21f49a375ea5162d81028942fe6a5b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-09 19:07:58 -0300

    Update form.blade.php

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,18 +1,29 @@
 @props([
     'route',
     'post' => null,
-    'put' => null
+    'put' => null,
+    'delete' => null,
+    'patch' => null
 ])
 
 @php
-    $method = $post || $put ? 'post' : 'get';
+    $method = $post || $put || $delete || $patch ? 'post' : 'get';
 @endphp
 
-<form {{ $attributes->class(['flex flex-col gap-4']) }} :action="$route" method="{{ $method }}">
+<form {{ $attributes->class(['flex flex-col gap-4']) }} action="{{ $route }}" method="{{ $method }}">
     @csrf
 
     @if ($put)
         @method('put')
+    @endif
+
+    @if ($delete)
+        @method('delete')
+    @endif
+
+
+    @if ($patch)
+        @method('patch')
     @endif
 
     {{ $slot }}
